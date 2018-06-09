3081d9dcf4cd2ee925e404c0dcf5f7143a7187d0
Renato Inacio <renato_inacio17@hotmail.com>
2018-06-09 13:00:42 -0400

    PUT, Delete, and Get to Client

--- src/main/java/com/dossanre/coursemc/resources/ClientResource.java
+++ src/main/java/com/dossanre/coursemc/resources/ClientResource.java
@@ -1,13 +1,22 @@
 package com.dossanre.coursemc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dossanre.coursemc.domain.Client;
+import com.dossanre.coursemc.dto.ClientDTO;
 import com.dossanre.coursemc.services.ClientService;
 
 @RestController
@@ -24,5 +33,42 @@ public class ClientResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	// Update Client
+		@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+		public ResponseEntity<Void> update(@Valid @RequestBody ClientDTO categDto, @PathVariable Integer id){
+			Client categ = service.fromDTO(categDto);
+			categ.setId(id);
+			categ = service.update(categ);
+			return ResponseEntity.noContent().build();
+		}
+		// Delete Client
+		@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+		public ResponseEntity<Void> delete(@PathVariable Integer id){
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		}
+
+		@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<List<ClientDTO>> findAll(){
+			List<Client> listCateg = service.findAll();
+			List<ClientDTO> listDto = listCateg.stream().map(obj-> new ClientDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
+		}
+		// Using GET 
+		//http://localhost:8080/category/page?linesPerPage=3
+		//http://localhost:8080/category/page?page=1&linesPerPage=3&direction=DESC
+		@RequestMapping(value="/page",method=RequestMethod.GET)
+		public ResponseEntity<Page<ClientDTO>> findPage(
+				@RequestParam(value="page", defaultValue="0") Integer page,
+				@RequestParam(value="linesPerPage", defaultValue ="24") Integer linesPerPage,
+				@RequestParam(value="orderBy", defaultValue ="name")String orderBy,
+				@RequestParam(value="direction", defaultValue ="ASC")String direction){
+			Page<Client> listCateg = service.findPage(page, linesPerPage, orderBy, direction);
+			Page<ClientDTO> listDto = listCateg.map(obj-> new ClientDTO(obj));
+			return ResponseEntity.ok().body(listDto);
+		}
+		
+	
 
 }
--- src/main/java/com/dossanre/coursemc/services/ClientService.java
+++ src/main/java/com/dossanre/coursemc/services/ClientService.java
@@ -1,12 +1,19 @@
 package com.dossanre.coursemc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dossanre.coursemc.domain.Client;
+import com.dossanre.coursemc.dto.ClientDTO;
 import com.dossanre.coursemc.repositories.ClientRepository;
+import com.dossanre.coursemc.services.exceptions.DataIntegrityException;
 import com.dossanre.coursemc.services.exceptions.ObjectNotFoundException;
 
 
@@ -22,6 +29,44 @@ public class ClientService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(    
 				"Client Not Found! Id: " + id + ", Type: " + Client.class.getName())); 
 		
+	}
+	
+	public Client update(Client client) {
+		Client newClient = find(client.getId());
+		updateData(newClient, client);
+		return repo.save(newClient);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Can not delete a client with orders.");
+		}
+	}
+	
+	public List<Client> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Client> findPage(Integer page, Integer linePerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
+	}
+	
+	public Client fromDTO(ClientDTO clientDTO ) {
+		
+		return new Client(clientDTO.getId(), clientDTO.getName(),clientDTO.getEmail(), null, null);
+		
+	}
+	
+	private void updateData(Client newClient, Client client) {
+		newClient.setName(client.getName());
+		newClient.setEmail(client.getName());
 	}
 
 }
